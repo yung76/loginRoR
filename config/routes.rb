56f4550c623b1f9bc8154dfb06f690609231737a
yung57 <flr.your@gmail.com>
2023-11-14 17:35:00 -0300

    login y crud

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
   root "articles#index"
 
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
+resources :articles
 end
